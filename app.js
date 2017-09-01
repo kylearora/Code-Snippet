77ef78187db4ea39d5975d6fe81f79264a1ba22c
kylearora <kylearora@gmail.com>
2017-09-01 11:56:59 -0700

    Initial Commit - Created files and added modules

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+const session = require("express-session")
+const mongooseSession = require("mongoose-session")
+const mustache = require("mustache-express")
+const bodyParser = require("body-parser")
+const MongoClient = require("mongodb")
+const mongoose = require("mongoose")
+mongoose.Promise = require("bluebird")
+app.engine('mustache', mustache())
+app.set('view engine', 'mustache')
+app.use( express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+const routeApp = require("./routes/main")
+app.use(routeApp)
+
+
+app.listen(3000, function(){
+  console.log("Dang BRO we are live!")
+})
