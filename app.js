ed86c47aa09dd2c2dad06f07ce33a9a9906fa5c8
kylearora <kylearora@gmail.com>
2017-09-01 13:53:14 -0700

    Created login page, schema, and database and added functionality to store new registered users to database

--- app.js
+++ app.js
@@ -9,14 +9,16 @@ const mongoose = require("mongoose")
 mongoose.Promise = require("bluebird")
 app.engine('mustache', mustache())
 app.set('view engine', 'mustache')
-app.use( express.static('public'))
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 
+const DBUrl = "mongodb://127.0.0.1:27017/snippet"
+mongoose.connect(DBUrl)
 
 const routeApp = require("./routes/main")
 app.use(routeApp)
 
 
 app.listen(3000, function(){
-  console.log("Dang BRO we are live!")
+  console.log("Dang BRO, we are live!")
 })
--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,49 @@
 const express = require("express")
 const router = express.Router()
-
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+// mongoose.Promise = require("bluebird")
+const Users = require("../models/Users")
+// const DBUrl = "mongodb://127.0.0.1:27017/snippet"
+// mongoose.connect(DBUrl)
 
 router.get("/", function(req, res){
-  res.render("index")
+  res.redirect("/welcome")
+})
+
+router.get("/welcome", function(req, res){
+  res.render("welcome")
+})
+
+router.get("/login", function(req, res){
+  res.render("login")
+})
+
+router.get("/register", function(req,res){
+  res.render("register")
+})
+
+router.post("/register", function(req,res) {
+  const name = req.body.name
+  const email= req.body.email
+  const username = req.body.username
+  const password = req.body.password
+  const user = new Users()
+  user.name = name
+  user.email = email
+  user.username = username
+  user.passwordHash = bcrypt.hashSync(password, 8)
+  user.save().then(function(user) {
+    // req.session.user = user
+    res.redirect("/")
+  })
+  .catch(function(error){
+    res.render("register", {
+      user:user,
+      errorMessage: "You need more info, DAWG",
+      error: error.errors
+    })
+  })
 })
 
 module.exports = router
